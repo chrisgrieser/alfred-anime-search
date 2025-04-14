8424255e0f06215f58e10cae2592dd3b8ec0ca5c
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-04-14 15:07:37 +0200

    feat: `r/anime` seasonal weekly ranking

--- README.md
+++ README.md
@@ -1,42 +1,50 @@
 # Alfred Anime Search
-![GitHub Downloads](https://img.shields.io/github/downloads/chrisgrieser/alfred-anime-search/total?label=GitHub%20Downloads&style=plastic&logo=github)
-![Alfred Gallery Downloads](https://img.shields.io/badge/dynamic/yaml?url=https%3A%2F%2Fraw.githubusercontent.com%2Fchrisgrieser%2F.config%2Frefs%2Fheads%2Fmain%2FAlfred.alfredpreferences%2Falfred-workflow-download-count.yaml&query=alfred-anime-search&style=plastic&logo=alfred&label=Gallery%20Downloads&color=%235C1F87)
-![Latest Release](https://img.shields.io/github/v/release/chrisgrieser/alfred-anime-search?label=Latest%20Release&style=plastic)
+![GitHub downloads](https://img.shields.io/github/downloads/chrisgrieser/alfred-anime-search/total?label=GitHub%20Downloads&style=plastic&logo=github)
+![Alfred gallery downloads](https://img.shields.io/badge/dynamic/yaml?url=https%3A%2F%2Fraw.githubusercontent.com%2Fchrisgrieser%2F.config%2Frefs%2Fheads%2Fmain%2FAlfred.alfredpreferences%2Falfred-workflow-download-count.yaml&query=alfred-anime-search&style=plastic&logo=alfred&label=Gallery%20Downloads&color=%235C1F87)
+![Latest release](https://img.shields.io/github/v/release/chrisgrieser/alfred-anime-search?label=Latest%20Release&style=plastic)
 
-Search animes listed in the [myanimelist](http://myanimelist.net) database.
+Search animes listed in the [myanimelist](http://myanimelist.net) database or
+the [r/anime](https://www.reddit.com/r/anime/) weekly seasonal rankings.
+
 Open at [myanimelist](http://myanimelist.net) or [AniList](https://anilist.co).
 
 <img alt="Showcase" width=70% src="https://github.com/chrisgrieser/alfred-anime-search/assets/73286100/a84fc03b-e7f0-4926-8c04-df73cd53c813">
 
 ## Usage
 Search all anime listed in the `myanimelist` database via the keyword `an`.
-- <kbd>⏎</kbd>: Open at `myanimelist` or `AniList`.
+- <kbd>⏎</kbd>: Open at myanimelist.net or AniList.
 - <kbd>⌘⏎</kbd>: Copy the Japanese (romaji) title.
-- <kbd>⌥⏎</kbd>: Copy the `myanimelist` or `AniList` URL.
+- <kbd>⌥⏎</kbd>: Copy the myanimelist.net or AniList URL.
 - <kbd>⇧⏎</kbd>: Search for the title on an alternative site, such as a
   streaming website.
-- <kbd>⌘Y</kbd>: Preview the hero image or `myanimelist` page of the entry.
+- <kbd>⌘Y</kbd>: Quick Look the hero image or `myanimelist` page of the entry.
+
+Access the [r/anime](https://www.reddit.com/r/anime/) weekly seasonal rankings
+via `as`.
+- <kbd>⏎</kbd>: Open weekly episode discussion thread at reddit.
+- <kbd>⌘⏎</kbd>: Open the show at myanimelist.net or AniList.
+- <kbd>⇧⏎</kbd>: Open the episode at the streaming site, if available.
+- <kbd>⌘Y</kbd>: Quick Look the hero image of the show/episode.
 
 ## Installation
 [➡️ Download the latest release.](https://github.com/chrisgrieser/alfred-anime-search/releases/latest)
 
-<!-- vale Google.FirstPerson = NO -->
+## Credits
+- Anime data by [Jikan](https://docs.api.jikan.moe/).
+- `r/anime` seasonal rankings by
+  [abysswatcherbel](https://github.com/abysswatcherbel/abysswatcherbel.github.io).
+
 ## About the developer
 In my day job, I am a sociologist studying the social mechanisms underlying the
 digital economy. For my PhD project, I investigate the governance of the app
 economy and how software ecosystems manage the tension between innovation and
 compatibility. If you are interested in this subject, feel free to get in touch.
 
-- [Academic Website](https://chris-grieser.de/)
+- [Website](https://chris-grieser.de/)
 - [Mastodon](https://pkm.social/@pseudometa)
 - [ResearchGate](https://www.researchgate.net/profile/Christopher-Grieser)
 - [LinkedIn](https://www.linkedin.com/in/christopher-grieser-ba693b17a/)
 
-<a href='https://ko-fi.com/Y8Y86SQ91' target='_blank'>
-	<img
-	height='36'
-	style='border:0px;height:36px;'
-	src='https://cdn.ko-fi.com/cdn/kofi1.png?v=3'
-	border='0'
-	alt='Buy Me a Coffee at ko-fi.com'
-/></a>
+<a href='https://ko-fi.com/Y8Y86SQ91' target='_blank'> <img height='36'
+style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi1.png?v=3'
+border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
---
+++ scripts/weekly-seasonal-ranking.js
@@ -0,0 +1,137 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @param {string} url @return {string} */
+function httpRequest(url) {
+	const queryURL = $.NSURL.URLWithString(url);
+	const data = $.NSData.dataWithContentsOfURL(queryURL);
+	return $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding).js;
+}
+
+/** @typedef {Object} rAnimeRanking
+ * @property {string} title
+ * @property {string} title_english
+ * @property {string} url // reddit url
+ * @property {number} karma
+ * @property {number} karma_change
+ * @property {number} current_rank
+ * @property {number|string} rank_change // string for "returning" and such
+ * @property {number} mal_id
+ * @property {number} episode
+ * @property {number} comments
+ * @property {{large: string, medium: string}?} images
+ * @property {{name: string, url: string, logo: string}?} streams
+ */
+
+//──────────────────────────────────────────────────────────────────────────────
+
+// SOURCE https://github.com/abysswatcherbel/abysswatcherbel.github.io
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run() {
+	const useJapTitle = $.getenv("seasonal_jap_title") === "1";
+
+	// get year/season/weekNum based on files available
+	const year = new Date().getFullYear();
+	/** @type {Record<string, number>} */
+	const seasons = { winter: 0, spring: 0, summer: 0, fall: 0 };
+	const treeUrl =
+		"https://api.github.com/repos/abysswatcherbel/abysswatcherbel.github.io/git/trees/main?recursive=1";
+	const tree = JSON.parse(httpRequest(treeUrl))?.tree;
+	for (const file of tree) {
+		const [_, seas, week] = file.path.match(/docs\/static\/data\/(\w+)\/week_(\d+)\.json/) || [];
+		if (!seas || !week) continue;
+		seasons[seas] = Math.max(seasons[seas], Number(week));
+	}
+	let season = "fall";
+	if (seasons.fall === 0) season = "summer";
+	if (seasons.summer === 0) season = "spring";
+	if (seasons.spring === 0) season = "winter";
+	const weekNum = seasons[season];
+
+	// get weekly seasonal ranking
+	const baseURL =
+		"https://raw.githubusercontent.com/abysswatcherbel/abysswatcherbel.github.io/refs/heads/main/docs/static/data/";
+	const weeklyURL = `${baseURL}${year}/${season}/week_${weekNum}.json`;
+	console.log("Weekly seasonal ranking URL:", weeklyURL);
+
+	// construct alfred items
+	let totalKarma = 0;
+
+	const response = JSON.parse(httpRequest(weeklyURL));
+	/** @type {AlfredItem[]} */
+	const items = response.map((/** @type {rAnimeRanking} */ show) => {
+		totalKarma += show.karma; // NOTE side effect, buf simpler this way
+		let rankChange = "";
+		if (typeof show.rank_change === "number" && show.rank_change !== 0) {
+			rankChange = (show.rank_change > 0 ? "📈" : "📉") + " " + show.rank_change.toString();
+		} else if (show.rank_change === 0) {
+			rankChange = "🟰";
+		} else if (show.rank_change === "returning") {
+			rankChange = "🔁";
+		} else if (show.rank_change === "new") {
+			rankChange = "🆕";
+		}
+		const karmaChange = show.karma_change ? ` (${show.karma_change})` : "";
+
+		const ranking = (show.current_rank + ")").padEnd(3, " ");
+		const title = (useJapTitle ? show.title : show.title_english).replace(
+			/ Season (\d+)$/,
+			" S$1",
+		);
+
+		const subtitle = [
+			"E" + show.episode.toString().padEnd(2, " "),
+			"🔼 " + show.karma + karmaChange,
+			"💬 " + show.comments,
+			rankChange,
+		].join("    ");
+
+		/** @type {AlfredItem} */
+		const alfredItem = {
+			title: ranking + " " + title,
+			subtitle: subtitle,
+			arg: show.url, // reddit URL
+			mods: {
+				cmd: {
+					arg: show.mal_id,
+					subtitle: "⌘: Open at " + $.getenv("open_at"),
+					valid: Boolean(show.mal_id),
+				},
+				shift: {
+					arg: show.streams?.url,
+					subtitle: show.streams?.url
+						? "⇧: Open at " + show.streams?.name
+						: "⇧: No stream available",
+					valid: Boolean(show.streams?.url),
+				},
+			},
+			quicklookurl: show.images?.large || show.images?.medium,
+		};
+		return alfredItem;
+	});
+
+	// add info as first item
+	const seasonCapitalized = season.charAt(0).toUpperCase() + season.slice(1);
+	const totalKarmaInfo = "Total karma: " + totalKarma.toLocaleString();
+	items.unshift({
+		title: `r/anime: ${seasonCapitalized} ${year}, week #${weekNum}`,
+		subtitle: totalKarmaInfo,
+		valid: false,
+		mods: {
+			shift: { subtitle: totalKarmaInfo, valid: false },
+			cmd: { subtitle: totalKarmaInfo, valid: false },
+		},
+	});
+
+	return JSON.stringify({
+		items: items,
+		skipknowledge: true, // keep ranking order
+		cache: {
+			seconds: 3600 * 3, // 3 hours
+			loosereload: true,
+		},
+	});
+}
